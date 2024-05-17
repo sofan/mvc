6054867e7f795c945c8b5bfaeac0570464b02ed5
sofan1 <soffan.s@gmail.com>
2024-05-17 07:19:08 +0200

    completed CardControllerJaonTest

--- tests/Controller/CardControllerJsonTest.php
+++ tests/Controller/CardControllerJsonTest.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-
 use App\Card\CardGraphic;
 use App\Card\DeckOfCards;
 
@@ -73,7 +72,8 @@ class CardControllerJsonTest extends WebTestCase
      *
      * @return void
      */
-    public function testDeckShufflePost() {
+    public function testDeckShufflePost()
+    {
 
         $client = static::createClient();
 
@@ -92,7 +92,12 @@ class CardControllerJsonTest extends WebTestCase
     }
 
 
-    public function testApiDeckShuffleGet()
+    /**
+     * test apiDeckShuffleGet
+     *
+     * @return void
+     */
+    public function testapiDeckDrawPost(): void
     {
         // Skapa en klient för att simulera en webbläsare
         $client = static::createClient();
@@ -111,12 +116,173 @@ class CardControllerJsonTest extends WebTestCase
 
         // Hämta och dekoda JSON-innehållet
         $content = $response->getContent();
-        $data = json_decode($content, true);
+        $this->assertIsString($content);
 
-        // Kontrollera att JSON-innehållet innehåller en kortlek med 52 kort
-        $this->assertArrayHasKey('deckOfCards', $data);
-        $this->assertCount(52, $data['deckOfCards']);
+        if ($content !== '') {
+            $data = json_decode($content, true);
+            $this->assertIsArray($data);
+
+            // Kontrollera att JSON-innehållet innehåller en kortlek med 52 kort
+            $this->assertArrayHasKey('deckOfCards', $data);
+        }
+
     }
+
+
+    /**
+     * test GET route /api/deck/shuffle
+     *
+     * @return void
+     */
+    public function testDeckShuffleGet()
+    {
+
+        $client = static::createClient();
+
+        // Send POST request
+        $client->request('POST', '/api/deck/shuffle');
+
+        // Get response
+        $response = $client->getResponse();
+
+        // Check that response is a redirect
+        $this->assertTrue($response->isRedirect());
+
+        // Check that rout is correct
+        $this->assertEquals('/api/deck/shuffle', $response->headers->get('Location'));
+    }
+
+
+    /**
+     * Test draw card POST
+     *
+     * @return void
+     */
+    public function testDrawPost()
+    {
+
+        $client = static::createClient();
+
+        // Send POST request
+        $client->request('POST', '/api/deck/draw');
+
+        // Get response
+        $response = $client->getResponse();
+
+        // Check that response is a redirect
+        $this->assertTrue($response->isRedirect());
+
+        // Check that rout is correct
+        $this->assertEquals('/api/deck/draw/1', $response->headers->get('Location'));
+    }
+
+
+    /**
+     * test draw number of cards POST
+     *
+     * @return void
+     */
+    public function testDrawNumPost()
+    {
+
+        $client = static::createClient();
+
+        $numCards = 3;
+
+        // Skicka en POST till routen med rätt antal kort
+        $client->request('POST', '/api/deck/draw/' . $numCards, ['num_cards' => $numCards]);
+
+        // Get response
+        $response = $client->getResponse();
+
+        // Check that response is a redirect
+        $this->assertTrue($response->isRedirect());
+
+        // Check that rout is correct
+        $this->assertEquals('/api/deck/draw/3', $response->headers->get('Location'));
+    }
+
+
+
+    /**
+     * test draw number of cards POST
+     *
+     * @return void
+     */
+    public function testDrawNumGet()
+    {
+
+        $client = static::createClient();
+
+        $numCards = 3;
+
+        // Skicka en POST till routen med rätt antal kort
+        $client->request('GET', '/api/deck/draw/' . $numCards);
+
+        // Get response
+        $response = $client->getResponse();
+
+        // Check that response is a JSON
+        $this->assertInstanceOf(JsonResponse::class, $response);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+    }
+
+
+    /**
+     * test card hand POST
+     *
+     * @return void
+     */
+    public function testApiCardHandPost()
+    {
+
+        $client = static::createClient();
+
+        // Skicka en POST till routen med rätt antal kort
+        $client->request('POST', '/api/deck/deal/2/3');
+
+        // Get response
+        $response = $client->getResponse();
+
+        // Check that response is a redirect
+        $this->assertTrue($response->isRedirect());
+
+        // Check that rout is correct
+        $this->assertEquals('/api/deck/deal/0/0', $response->headers->get('Location'));
+    }
+
+
+
+    /**
+     * test card hand GET
+     *
+     * @return void
+     */
+    public function testCardHandGet()
+    {
+
+        $client = static::createClient();
+
+        $numPlayers = 2;
+        $numCards = 5;
+
+        // Skicka en POST till routen med rätt antal kort
+        $client->request('GET', '/api/deck/deal/' . $numPlayers . '/' . $numCards);
+
+        // Get response
+        $response = $client->getResponse();
+
+        // Check that response is a JSON
+        $this->assertInstanceOf(JsonResponse::class, $response);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+    }
+
+
+
 
 
 
